d335085abce1ea00ecd8cd71e2656ae76c78dafb
App Academy Student <kjjafshf>
2018-03-06 18:02:55 -0500

    Work on session form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
 
 const App = () => (
   <div>
     <h1>SongCabin</h1>
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
   </div>
 );
 
---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      band_name: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="login-form-container">
+        {this.props.formType}
+        <form onSubmit={this.handleSubmit} className="login-form-box">
+          <br/>
+          {this.renderErrors()}
+          <div className="login-form">
+            <br/>
+            <label>Band name:
+              <input type="text"
+                value={this.state.band_name}
+                onChange={this.update('band_name')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <label>Password:
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <input className="session-submit" type="submit" value={this.props.formType} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SessionForm);
