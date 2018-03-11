1a555ac1af85d0ca152c93504b047d7afc2af7e7
Adam Schneit <aschneit@hotmail.com>
2018-03-11 01:29:41 -0500

    complete artist page show component

--- frontend/components/artist/artist_show.jsx
+++ frontend/components/artist/artist_show.jsx
@@ -15,6 +15,18 @@ export default class ArtistShow extends React.Component {
         <div className='artist-image'>
           <img src={this.props.artist.image_url}/>
         </div>
+        <div className="artist-show-name">
+          {this.props.artist.band_name}
+        </div>
+        <div className="artist-location">
+          {this.props.artist.band_location}
+        </div>
+        <div className="artist-description">
+          {this.props.artist.band_description}
+        </div>
+        <div className="artist-website">
+          <a href={`http://${this.props.artist.band_website}`} target='_new'>{this.props.artist.band_website}</a>
+        </div>
       </div>
 
     );
