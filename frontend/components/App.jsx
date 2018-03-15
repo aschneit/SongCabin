2e78e0b510b3216eafcea6f383b747d1fec0c9d4
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2018-03-15 14:07:22 -0400

    Testing audio

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -10,16 +10,26 @@ import ArtistPageContainer from './artist/artist_page_container';
 import EditArtistContainer from './artist/edit_artist_container';
 import CreateAlbumContainer from './album/create_album_container';
 
-const App = () => (
-  <div className = 'main'>
-    <Route path="/" component={MainNavContainer}/>
-    <Route exact path="/" component={MainPage}/>
-    <Route path="/artists/:artistId" component={ArtistPageContainer}/>
-    <AuthRoute path="/login" component={LoginFormContainer} />
-    <ProtectedRoute path="/artist-edit" component={EditArtistContainer} />
-    <ProtectedRoute path="/albums/new" component={CreateAlbumContainer} />
-    <AuthRoute path="/signup" component={SignupFormContainer} />
-  </div>
-);
+class App extends React.Component {
+   componentDidMount() {
+    window.thing = this.playerAudio;
+  }
+  render() {
+    return (
+      <div className = 'main'>
+        <audio ref={(audio) => this.playerAudio = audio} src={"http://localhost:3000/assets/Jenny-9b167fb60d83bbfbc54cb098e666b747df71ac267939b94b9864bc066e6cb263.mp3"}
+          onTimeUpdate={() => {console.log(this.playerAudio.currentTime)}}
+          ></audio>
+        <Route path="/" component={MainNavContainer}/>
+        <Route exact path="/" component={MainPage}/>
+        <Route path="/artists/:artistId" component={ArtistPageContainer}/>
+        <AuthRoute path="/login" component={LoginFormContainer} />
+        <ProtectedRoute path="/artist-edit" component={EditArtistContainer} />
+        <ProtectedRoute path="/albums/new" component={CreateAlbumContainer} />
+        <AuthRoute path="/signup" component={SignupFormContainer} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- frontend/components/album/song_player3.jsx
+++ frontend/components/album/song_player3.jsx
@@ -7,68 +7,31 @@ class SongPlayer extends React.Component {
     super(props);
     this.state = {
       playing: false,
-      slider: 0,
+      slider: 0
     };
     this.handlePlay = this.handlePlay.bind(this);
-    this.slider = this.slider.bind(this);
     this.moveSlider = this.moveSlider.bind(this);
-    // this.handleSliderChange = this.handleSliderChange.bind(this);
-    this.changeTime = this.changeTime.bind(this);
   }
 
   moveSlider() {
     this.setState({slider: this.slider()});
   }
 
-  componentDidMount() {
-  }
-
-  componentDidUpdate() {
-    // debugger
-    window.weirdName = this.playerAudio;
-
-  }
-
-
   handlePlay(e) {
-
 
     if (this.state.playing === false)
       {
         this.playerAudio.play();
         this.setState({playing: true});
-
       } else {
         this.playerAudio.pause();
         this.setState({playing: false});
-
       }
   }
 
   slider () {
     return (this.playerAudio.currentTime / this.playerAudio.duration * 250);
   }
-
-  // handleSliderChange (e) {
-  //   const time = this.state.slider / 250 * this.playerAudio.duration;
-  //   this.setState({time: time});
-  //
-  // }
-
- handleSliderChange (e) {
-   // this.setState({slider: e.currentTarget.value});
-   console.log('changed');
-   console.log(this.playerAudio);
-   this.playerAudio.currentTime = 200;
-   console.log(this.playerAudio.currentTime);
-
-
- }
-
- changeTime (e) {
-   this.playerAudio.currentTime = this.state.slider / 250 * this.playerAudio.duration;
- }
-
 
   render() {
     let icon;
@@ -82,9 +45,7 @@ class SongPlayer extends React.Component {
       <div>
         <div className="native-player">
         {this.props.leadTrack[0] &&
-        <audio ref={(audio) => this.playerAudio = audio} src={leadTrack.audio_url}
-          onTimeUpdate={() => {console.log(this.playerAudio.currentTime)}}
-          ></audio>
+        <audio ref={(audio) => { this.playerAudio = audio; }} src={leadTrack.audio_url} onTimeUpdate={this.moveSlider}></audio>
         }
         </div>
         <div className="player-total">
@@ -97,7 +58,9 @@ class SongPlayer extends React.Component {
             <div className="current-track-title">
               {leadTrack.title}
             </div>
-
+            <div className="slider-container">
+              <input ref={(input) => { this.playerSlider = input; }} type="range" value={this.state.slider} min="1" max="250" className="slider" id="myRange"/>
+            </div>
           </div>
           <div className="player-advance">
           </div>
@@ -107,9 +70,5 @@ class SongPlayer extends React.Component {
     );
   }
 }
-
-
-
-
 
 export default withRouter(SongPlayer);
