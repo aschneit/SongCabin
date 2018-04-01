d38432f4bb52c43c1d8004939ce3484be9423fe0
Adam Schneit <aschneit@hotmail.com>
2018-04-01 19:19:27 -0400

    Work on logged in artist nav

--- frontend/components/artist/artist_nav.jsx
+++ frontend/components/artist/artist_nav.jsx
@@ -1,59 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/fontawesome-free-solid';
+import { faSearch, faCaretDown } from '@fortawesome/fontawesome-free-solid';
 import SearchBarContainer from '../main/search_bar_container.jsx';
 
+
+
+
+
 const loggedOutLinks = () => {
-  return (
-    <ul className="artist-nav-session-buttons">
-      <li className="artist-nav-right-link">
-        gift cards
-      </li>
-      <li className="artist-nav-right-link">
-        <Link to={"/signup"}>sign up</Link>
-      </li>
-      <li className="artist-nav-right-link">
-        <Link to={"/login"}>log in</Link>
-      </li>
-    </ul>
-  );
-};
-
-const loggedInLinks = logout => {
-  return (
-    <ul className="artist-nav-session-buttons">
-      <li className="artist-nav-discover">
-        discover
-      </li>
-      <li className="artist-nav-logout">
-        <button onClick={logout}>log out</button>
-      </li>
-    </ul>
-  );
-};
-
-const ArtistNav = ({ currentUser, logout, location }) => {
-
-  let links;
-  let greeting;
-  let yourSite;
-  if (currentUser) {
-    links = loggedInLinks(logout);
-    greeting = (
-      <li className="main-nav-greeting">Hi {currentUser.band_name}</li>
-    );
-      yourSite = (
-      <li className="main-nav-yoursite">
-        <Link to={`/artists/${currentUser.id}`}>your site</Link>
-      </li>
-    );
-  } else {
-    links = loggedOutLinks();
-    greeting = (
-      <li className="artist-nav-greet-unlogged">Discover great music and support independent artists.</li>
-    );
-  }
   return (
     <div className="artist-nav">
       <div className="artist-nav-bar">
@@ -68,9 +23,82 @@ const ArtistNav = ({ currentUser, logout, location }) => {
         <div className= "artist-nav-search">
           <SearchBarContainer />
         </div>
-        <div className="artist-nav-right">{links}
+        <div className="artist-nav-right">
+          <ul className="artist-nav-session-buttons">
+            <li className="artist-nav-right-link">
+              gift cards
+            </li>
+            <li className="artist-nav-right-link">
+              <Link to={"/signup"}>sign up</Link>
+            </li>
+            <li className="artist-nav-right-link">
+              <Link to={"/login"}>log in</Link>
+            </li>
+          </ul>
         </div>
       </div>
+    </div>
+  );
+};
+
+const loggedInLinks = (logout, currentUser) => {
+  return (
+    <div className="artist-nav">
+      <div className="artist-nav-bar">
+        <div className="artist-nav-logo logged">
+          <Link to={"/"}>
+            <img src={bc}/><span>songcabin</span>
+          </Link>
+        </div>
+        <div className = "artist-nav-addalbum">
+          <Link to={"/albums/new"}>
+            add album
+          </Link>
+        </div>
+        <div className = "artist-nav-dropdown" >
+          <Link to={"#"} className = "artist-nav-dropdown-link">
+            <div className = "artist-nav-dropdown-image">
+              <img src = {currentUser.image_url}/>
+            </div>
+            <span className = "artist-nav-dropdown-name">
+              {currentUser.band_name}
+            </span>
+            <span className = "artist-nav-dropdown-caret">
+              <FontAwesomeIcon  className ="dropdown-caret" icon={faCaretDown} />
+            </span>
+          </Link>
+          <div className = "artist-nav-dropdown-clicked">
+
+          </div>
+          {/*<ul className="artist-nav-session-buttons">
+
+            <li className="artist-nav-logout">
+              <button onClick={logout}>log out</button>
+            </li>
+          </ul> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ArtistNav = ({ currentUser, logout, location }) => {
+
+  let links;
+  let yourSite;
+  if (currentUser) {
+    links = loggedInLinks(logout, currentUser);
+      yourSite = (
+      <li className="main-nav-yoursite">
+        <Link to={`/artists/${currentUser.id}`}>your site</Link>
+      </li>
+    );
+  } else {
+    links = loggedOutLinks();
+  }
+  return (
+    <div>
+      {links}
     </div>
   );
 };
